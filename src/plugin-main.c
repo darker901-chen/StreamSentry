4366ad23be5aa85cd283b6c866f7446bc2ff23bc
darker <hahaha901@gmail.com>
2026-07-05 15:17:14 +0800

    Rename plugin identity from obsplugin to StreamSentry
    buildspec.json name/displayName/macOS bundleId, filter source id
    (streamsentry_filter), struct/function prefixes, and the localized
    FilterName string all updated. CMAKE_PROJECT_NAME (and therefore
    PLUGIN_NAME / the built DLL filename) follows buildspec.json name
    automatically. No behavior change.

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,5 +1,5 @@
 /*
-obsplugin
+StreamSentry
 Copyright (C) 2026 darker hahaha901@gmail.com
 
 This program is free software; you can redistribute it and/or modify
@@ -25,37 +25,37 @@ OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 /* M0 skeleton: register a video filter that passes the source through
  * untouched. No detection, no masking. */
 
-struct obsplugin_filter {
+struct streamsentry_filter {
 	obs_source_t *source;
 	bool enabled;
 };
 
-static const char *obsplugin_filter_get_name(void *unused)
+static const char *streamsentry_filter_get_name(void *unused)
 {
 	UNUSED_PARAMETER(unused);
 	return obs_module_text("FilterName");
 }
 
-static void obsplugin_filter_update(void *data, obs_data_t *settings)
+static void streamsentry_filter_update(void *data, obs_data_t *settings)
 {
-	struct obsplugin_filter *filter = data;
+	struct streamsentry_filter *filter = data;
 	filter->enabled = obs_data_get_bool(settings, "enabled");
 }
 
-static void *obsplugin_filter_create(obs_data_t *settings, obs_source_t *source)
+static void *streamsentry_filter_create(obs_data_t *settings, obs_source_t *source)
 {
-	struct obsplugin_filter *filter = bzalloc(sizeof(struct obsplugin_filter));
+	struct streamsentry_filter *filter = bzalloc(sizeof(struct streamsentry_filter));
 	filter->source = source;
-	obsplugin_filter_update(filter, settings);
+	streamsentry_filter_update(filter, settings);
 	return filter;
 }
 
-static void obsplugin_filter_destroy(void *data)
+static void streamsentry_filter_destroy(void *data)
 {
 	bfree(data);
 }
 
-static obs_properties_t *obsplugin_filter_get_properties(void *data)
+static obs_properties_t *streamsentry_filter_get_properties(void *data)
 {
 	UNUSED_PARAMETER(data);
 
@@ -64,35 +64,35 @@ static obs_properties_t *obsplugin_filter_get_properties(void *data)
 	return props;
 }
 
-static void obsplugin_filter_get_defaults(obs_data_t *settings)
+static void streamsentry_filter_get_defaults(obs_data_t *settings)
 {
 	obs_data_set_default_bool(settings, "enabled", true);
 }
 
-static void obsplugin_filter_video_render(void *data, gs_effect_t *effect)
+static void streamsentry_filter_video_render(void *data, gs_effect_t *effect)
 {
-	struct obsplugin_filter *filter = data;
+	struct streamsentry_filter *filter = data;
 
 	UNUSED_PARAMETER(effect);
 	obs_source_skip_video_filter(filter->source);
 }
 
-static struct obs_source_info obsplugin_filter_info = {
-	.id = "obsplugin_filter",
+static struct obs_source_info streamsentry_filter_info = {
+	.id = "streamsentry_filter",
 	.type = OBS_SOURCE_TYPE_FILTER,
 	.output_flags = OBS_SOURCE_VIDEO,
-	.get_name = obsplugin_filter_get_name,
-	.create = obsplugin_filter_create,
-	.destroy = obsplugin_filter_destroy,
-	.update = obsplugin_filter_update,
-	.get_properties = obsplugin_filter_get_properties,
-	.get_defaults = obsplugin_filter_get_defaults,
-	.video_render = obsplugin_filter_video_render,
+	.get_name = streamsentry_filter_get_name,
+	.create = streamsentry_filter_create,
+	.destroy = streamsentry_filter_destroy,
+	.update = streamsentry_filter_update,
+	.get_properties = streamsentry_filter_get_properties,
+	.get_defaults = streamsentry_filter_get_defaults,
+	.video_render = streamsentry_filter_video_render,
 };
 
 bool obs_module_load(void)
 {
-	obs_register_source(&obsplugin_filter_info);
+	obs_register_source(&streamsentry_filter_info);
 	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
 	return true;
 }
